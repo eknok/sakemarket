d8bd46a7223eb8dae663adaa0eee9e3c191a12fc
EikiKanaoka <kakakaeee@gmail.com>
2018-06-27 19:24:35 +0900

    View top#about

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,4 +2,8 @@ class TopController < ApplicationController
   def index
     @posts = Post.order("RANDOM()").limit(9)
   end
+
+  def about
+    @posts = Post.order('RANDOM()').limit(9)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root to: 'top#index'
+  get '/about', to: 'top#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
